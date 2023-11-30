d3d58b0dd4ef55547d179058484a267b284f4a05
Pippah Gift <Pippah.gift@gmail.com>
2023-11-30 14:03:47 -0800

    alx

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import sys
 import add_0
-a = int(sys.argv[1]) if len(sys.argv) > 1 else 1
-b = int(sys.argv[2]) if len(sys.argv) > 2 else 2
-result = add_0.add(a, b)
-string = f'{a}+ {b} = {result}'
-print(string)
+if __name__ == "__main__":
+    a = int(sys.argv[1]) if len(sys.argv) > 1 else 1
+    b = int(sys.argv[2]) if len(sys.argv) > 2 else 2
+    result = add_0.add(a, b)
+    string = f'{a}+ {b} = {result}'
+    print(string)
