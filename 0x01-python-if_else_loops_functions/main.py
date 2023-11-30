0e63f3ab0e55fe3cb86a3521a9158ee0db69a84f
Pippah Gift <Pippah.gift@gmail.com>
2023-11-29 18:15:05 -0800

    alx

--- 0x01-python-if_else_loops_functions/main.py
+++ 0x01-python-if_else_loops_functions/main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-pow = __import__('11-pow').pow
+add = __import__('10-add').add
 
-print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5))
+print(add(1, 2))
+print(add(98, 0))
+print(add(100, -2))
