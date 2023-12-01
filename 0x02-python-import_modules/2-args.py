936dc704415df9fbfe35604ccc59824bf214234c
Pippah Gift <Pippah.gift@gmail.com>
2023-11-30 20:33:29 -0800

    alx

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    index = len(sys.argv) - 1
+    if index == 0:
+        print("arguments")
+    elif index == 1:
+        printf("argument = 1:")
+    else:
+        print("{} arguments:".format(index))
+    for i in range(index):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
