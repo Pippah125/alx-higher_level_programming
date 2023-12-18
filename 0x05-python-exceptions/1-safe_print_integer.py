a157ec6e26e5cc76c622b60d9859cd0a0215f2fc
Pippah Gift <Pippah.gift@gmail.com>
2023-12-18 06:33:08 -0800

    alx

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except ValueError as e:
+    except(ValueError,TypeError) as e:
         return False
     else:
         return True
