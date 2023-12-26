1f911317e1d452891993234f0d44c9d923db8e78
Pippah Gift <Pippah.gift@gmail.com>
2023-12-26 10:26:59 -0800

    alx

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    new_string = ""
+    for a in range(len(str)):
+        if a == n:
+            continue
+        new_string = new_string + str[a]
+    return(new_string)
